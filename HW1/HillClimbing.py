26ac24a1156eaa9a38b0dd80c2438dacdd7aecae
Anda Buinoschi <anda.buinoschi@endava.com>
2020-03-09 16:19:45 +0200

    refactored the code to look more nicely

--- HW1/HillClimbing.py
+++ HW1/HillClimbing.py
@@ -1,7 +1,7 @@
 import numpy as np
 from math import sqrt, cos
 
-MAX = 200
+MAX = 50
 
 
 def switch_bit(bitarray, position):
@@ -10,108 +10,134 @@ def switch_bit(bitarray, position):
     return np.array(temp)
 
 
-def binary_decoding(binary_point, a, b):
-    decimal = 0
-    for index in range(0, len(binary_point)):
-        decimal += binary_point[index] * pow(2, len(binary_point) - index - 1)
-    real = a + decimal * (b - a) / (pow(2, len(binary_point)) - 1)
-    return real
-
-
-def return_float_point(candidate, n, a, b, number_variables):
-    candidate_array = [candidate[i * int(n):(i + 1) * int(n)] for i in range(0, number_variables)]
-    candidate_np_array = np.array(candidate_array)
-    # print(candidate_np_array, candidate_np_array.shape)
-    float_points = np.zeros(number_variables)
-    for index in range(0, number_variables):
-        float_points[index] = binary_decoding(candidate_np_array[index], a, b)
-    return float_points
-
-
-def generate_initial_candidate(n, number_variables):
-    candidate = np.zeros(shape=number_variables * n)
-    for i in range(0, number_variables * n):
-        bit = np.round(np.random.uniform(0, 1))
-        candidate[i] = bit
-    return candidate
-
-
-def generate_HammingNeighbours(candidate, n, number_variables):
-    candidate_neighbours = np.zeros(shape=(n * number_variables, n * number_variables))
-    for index in range(0, n * number_variables):
-        candidate_neighbours[index] = switch_bit(candidate, index)
-    return candidate_neighbours
-
-# garbage code
 class HillClimbing:
-    def __init__(self):
-        self.precision = 4
-
-    def return_N_n(self, a, b):
-        N = (b - a) * pow(10, self.precision)
+    def return_N_n(self):
+        N = (self.b1 - self.a1) * pow(10, self.precision)
         n = np.ceil(np.log2(N))
         return int(N), int(n)
 
-    def test_Griewangks(self, candidate, number_variables):
-        a = -600.0
-        b = 600.0
-        N, n = self.return_N_n(a, b)
-        # print(N, n)
-        float_points = return_float_point(candidate, n, a, b, number_variables)
+    def __init__(self, function_type="Griewangk"):
+        self.precision = 4
+        if function_type == "Griewangk":
+            self.name = "Griewangk"
+            self.number_variables = 30
+            self.a1 = -600
+            self.b1 = 600
+            self.N, self.n = self.return_N_n()
+
+    def binary_decoding(self, binary_point):
+        decimal = 0
+        for index in range(0, len(binary_point)):
+            decimal += binary_point[index] * pow(2, len(binary_point) - index - 1)
+        real = self.a1 + decimal * (self.b1 - self.a1) / (pow(2, len(binary_point)) - 1)
+        return real
+
+    def generate_initial_candidate(self):
+        candidate = np.zeros(shape=self.number_variables * self.n)
+        for i in range(0, self.number_variables * self.n):
+            bit = np.round(np.random.uniform(0, 1))
+            candidate[i] = bit
+        return candidate
+
+    def return_float_point(self, candidate):
+        candidate_array = [
+            candidate[i * int(self.n) : (i + 1) * int(self.n)]
+            for i in range(0, self.number_variables)
+        ]
+        candidate_np_array = np.array(candidate_array)
+        # print(candidate_np_array, candidate_np_array.shape)
+        float_points = np.zeros(self.number_variables)
+        for index in range(0, self.number_variables):
+            float_points[index] = self.binary_decoding(candidate_np_array[index])
+        return float_points
+
+    def generate_HammingNeighbours(self, candidate):
+        candidate_neighbours = np.zeros(
+            shape=(self.n * self.number_variables, self.n * self.number_variables)
+        )
+        for index in range(0, self.n * self.number_variables):
+            candidate_neighbours[index] = switch_bit(candidate, index)
+        return candidate_neighbours
+
+    def evaluate(self, candidate):
+        float_points = self.return_float_point(candidate)
         # print(float_points)
-        sum = 0
-        for float_point in float_points:
-            sum += pow(float_point, 2)
-        function_termen_1 = sum / 4000
+        result = np.inf
+        if self.name == "Griewangk":
+            sum = 0
+            for float_point in float_points:
+                sum += pow(float_point, 2)
+            function_termen_1 = sum / 4000
 
-        function_termen_2 = 1
-        for index in range(0, number_variables):
-            function_termen_2 *= cos(float_points[index] / sqrt(index + 1))
+            function_termen_2 = 1
+            for index in range(0, self.number_variables):
+                function_termen_2 *= cos(float_points[index] / sqrt(index + 1))
 
-        result = function_termen_1 - function_termen_2 + 1
+            result = function_termen_1 - function_termen_2 + 1
         return result
 
-    def select_v_n_from_neighbours(self, val_v_c, neighbours, number_variables, method="first_improvement"):
+    def select_v_n_from_neighbours(
+        self, val_v_c, neighbours, method="first_improvement"
+    ):
         if method == "first_improvement":
             for index in range(0, neighbours.shape[0]):
-                if self.test_Griewangks(neighbours[index], number_variables) < val_v_c:
+                if self.evaluate(neighbours[index]) < val_v_c:
                     return neighbours[index]
+            return None
         elif method == "best_improvement":
             val_neighbours = []
             for neighbour in neighbours:
-                val_neighbours.append(self.test_Griewangks(neighbour, number_variables))
+                val_neighbours.append(self.evaluate(neighbour))
             index = np.argmin(val_neighbours)
             return neighbours[index]
 
-    def HillClimbingAlgorithm(self, n, number_variables):
+    def HillClimbingAlgorithm(self):
         t = 0
-        best = [1 for index in range(0, n * number_variables)]
-        best_value = self.test_Griewangks(best, number_variables)
+        best = [1 for index in range(0, self.n * self.number_variables)]
+        best_value = self.evaluate(best)
         while t < MAX:
+            print(
+                "---------------------------- ITERATIA {0} ----------------------------".format(
+                    t
+                )
+            )
             local = False
-            v_c = generate_initial_candidate(n, number_variables)
-            val_v_c = self.test_Griewangks(v_c, number_variables)
+            v_c = self.generate_initial_candidate()
+            val_v_c = self.evaluate(v_c)
             while not local:
-                neighbours = generate_HammingNeighbours(v_c, n, number_variables)
-                # v_n = self.select_v_n_from_neighbours(val_v_c,neighbours, number_variables, method="first_improvement")
-                v_n = self.select_v_n_from_neighbours(val_v_c, neighbours, number_variables, method="best_improvement")
-                val_v_n = self.test_Griewangks(v_n, number_variables)
+                neighbours = self.generate_HammingNeighbours(v_c)
+                v_n = self.select_v_n_from_neighbours(
+                    val_v_c, neighbours, method="first_improvement"
+                )
+                # v_n = self.select_v_n_from_neighbours(val_v_c, neighbours, method="best_improvement")
+                if v_n is not None:
+                    val_v_n = self.evaluate(v_n)
+                else:
+                    val_v_n = val_v_c
                 if val_v_n < val_v_c:
                     v_c = v_n
                     val_v_c = val_v_n
-                    print("Am gasit o valoare mai buna: {0}".format(self.test_Griewangks(v_n, number_variables)))
+                    print(
+                        "Am gasit o valoare mai buna: {0}".format(
+                            self.evaluate(v_n)
+                        )
+                    )
                 else:
                     local = True
             t += 1
             if val_v_c < best_value:
                 best = v_c
                 best_value = val_v_c
-            print("Iteratia {0}, cea mai buna valoare gasita: {1}".format(t, best_value))
-        return best
+            print(
+                "Iteratia {0}, cea mai buna valoare gasita: {1}".format(t, best_value)
+            )
+        print(
+            "Cea mai buna valoare gasita dupa {0} rulari este: {1}.".format(
+                MAX, best_value
+            )
+        )
 
 
 if __name__ == "__main__":
-    h = HillClimbing()
-    print(h.return_N_n(-600, 600))
-    print(h.HillClimbingAlgorithm(24, 30))
-    # print(h.generate_HammingNeighbours([1, 0,0,0,1,1],3,2))
+    h = HillClimbing("Griewangk")
+    h.HillClimbingAlgorithm()
