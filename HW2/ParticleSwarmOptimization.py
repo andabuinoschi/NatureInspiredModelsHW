39a1016eb4fbf91313a2b408082eb806e5b42877
Anda Buinoschi <anda.buinoschi@endava.com>
2020-05-04 22:43:44 +0300

    updated pso

--- HW2/ParticleSwarmOptimization.py
+++ HW2/ParticleSwarmOptimization.py
@@ -1,6 +1,7 @@
 import random
 
 import numpy as np
+from copy import copy
 
 from math import sqrt, cos, pi
 
@@ -8,7 +9,7 @@ from math import sqrt, cos, pi
 class ParticleSwarmOptimization:
     def __init__(self, function_type="Griewangk"):
         self.maximum_lookback_steps = 50
-        self.precision = 5
+        self.precision = 9
         self.number_particles = 60
         self.inertia_factor_w1 = 0.7
         self.particle_self_confidence_factor_w2 = 2
@@ -41,7 +42,7 @@ class ParticleSwarmOptimization:
         self.particles_positions_x = np.zeros(shape=(self.number_particles, self.number_variables))
         self.particles_velocities_v = np.zeros(shape=(self.number_particles, self.number_variables))
         self.generate_initial_particles()
-        self.best_particle_g = self.particles_positions_x[random.randint(0, self.number_particles)]
+        self.best_particle_g = copy(self.particles_positions_x[random.randint(0, self.number_particles)])
 
     def generate_initial_particles(self):
         if self.name != "SixHumpCamelBack":
@@ -56,7 +57,7 @@ class ParticleSwarmOptimization:
                 self.particles_velocities_v[index_particle][0] = random.uniform(self.a1, self.b1)
                 self.particles_positions_x[index_particle][1] = random.uniform(self.a2, self.b2)
                 self.particles_velocities_v[index_particle][1] = random.uniform(self.a2, self.b2)
-        self.particles_best_position_p = self.particles_positions_x
+        self.particles_best_position_p = copy(self.particles_positions_x)
 
     def evaluate(self, particle):
         result = np.inf
@@ -92,35 +93,72 @@ class ParticleSwarmOptimization:
             result = termen1 + termen2 + termen3
         return result
 
+    def update_velocities(self):
+        for i in range(0, self.number_particles):
+            for j in range(0, self.number_variables):
+                r1 = random.uniform(0, 1)
+                r2 = random.uniform(0, 1)
+                cognitive_factor = self.inertia_factor_w1 * r1 * (self.particles_best_position_p[i][j] - self.particles_positions_x[i][j])
+                social_factor = self.swarm_confidence_factor_w3 * r2 * (self.best_particle_g[j] - self.particles_positions_x[i][j])
+                self.particles_velocities_v[i][j] = self.inertia_factor_w1 * self.particles_velocities_v[i][j] + cognitive_factor + social_factor
+
+    def update_position(self):
+        for i in range(0, self.number_particles):
+            self.particles_positions_x[i] = self.particles_positions_x[i] + self.particles_velocities_v[i]
+            if self.name != "SixHumpCamelBack":
+                for j in range(0, self.number_variables):
+                    # adjust maximum position if necessary
+                    if self.particles_positions_x[i][j] > self.b1:
+                        self.particles_positions_x[i][j] = self.b1
+
+                    # adjust minimum position if neseccary
+                    if self.particles_positions_x[i][j] < self.a1:
+                        self.particles_positions_x[i][j] = self.a1
+            elif self.name == "SixHumpCamelBack":
+                if self.particles_positions_x[i][0] > self.b1:
+                    self.particles_positions_x[i][0] = self.b1
+                if self.particles_positions_x[i][0] < self.a1:
+                    self.particles_positions_x[i][0] = self.a1
+                if self.particles_positions_x[i][1] > self.b2:
+                    self.particles_positions_x[i][1] = self.b2
+                if self.particles_positions_x[i][1] < self.a2:
+                    self.particles_positions_x[i][1] = self.a2
+
     def PSOAlgorithm(self):
         t = 0
         best_particle_value_history = []
-        while True:  # conditie de oprire
+        local = False
+        while not local:  # conditie de oprire
             t = t + 1
-            random_p = random.uniform(0, 1)
-            random_g = random.uniform(0, 1)
-            self.particles_velocities_v = self.inertia_factor_w1 * self.particles_velocities_v + \
-                self.particle_self_confidence_factor_w2 * random_p * (self.particles_best_position_p - self.particles_positions_x) + \
-                self.swarm_confidence_factor_w3 * random_g * (self.best_particle_g - self.particles_positions_x)
-            self.particles_positions_x += self.particles_velocities_v
-            for index_particle in range (0, self.number_particles):
-                if self.evaluate(self.particles_positions_x[index_particle]) < self.evaluate(self.particles_best_position_p[index_particle]):
-                    self.particles_best_position_p[index_particle] = self.particles_positions_x[index_particle]
+            self.update_velocities()
+            self.update_position()
+            for index_particle in range(0, self.number_particles):
+                if self.evaluate(self.particles_positions_x[index_particle]) < self.evaluate(
+                        self.particles_best_position_p[index_particle]):
+                    self.particles_best_position_p[index_particle] = copy(self.particles_positions_x[index_particle])
             for index_particle in range(0, self.number_particles):
                 if self.evaluate(self.particles_best_position_p[index_particle]) < self.evaluate(self.best_particle_g):
-                    self.best_particle_g = self.particles_best_position_p[index_particle]
-                    # best_particle_value_history.append(self.best_particle_g)
-                    # if len(best_particle_value_history) == self.maximum_lookback_steps:
-                    #     for index in range (0, self.maximum_lookback_steps):
+                    self.best_particle_g = copy(self.particles_best_position_p[index_particle])
+                    # if len(best_particle_value_history) != self.maximum_lookback_steps:
+                    #     best_particle_value_history.append(self.best_particle_g)
+                    # else:
+                    #     deltas = [abs(self.evaluate(self.best_particle_g) - self.evaluate(best_particle_value_history[index])) for index in range (0, self.maximum_lookback_steps)]
+                    #     best_particle_value_history.pop(0)
+                    #     if all(delta <= pow(10, - self.precision) for delta in deltas):
+                    #         print("Cea mai buna valoare gasita pana la iteratia t = {0} este {1} si are coordonatele {2}".format(t,self.evaluate(self.best_particle_g),self.best_particle_g))
+                    #         local = True
             print("Cea mai buna valoare gasita pana la iteratia t = {0} este {1} si are coordonatele {2}".format(t,
-                self.evaluate(self.best_particle_g), self.best_particle_g))
+                                                                                                                 self.evaluate(
+                                                                                                                     self.best_particle_g),
+                                                                                                                 self.best_particle_g))
 
 
 if __name__ == "__main__":
     # pso = ParticleSwarmOptimization("Griewangk")
-    pso = ParticleSwarmOptimization("Rastrigin")
-    # pso = ParticleSwarmOptimization("Rosenbrock")
+    # pso = ParticleSwarmOptimization("Rastrigin")
+    pso = ParticleSwarmOptimization("Rosenbrock")
     # pso = ParticleSwarmOptimization("SixHumpCamelBack")
-    # pso.PSOAlgorithm()
-    # print(pso.particles_best_position_p)
     pso.PSOAlgorithm()
+    # last_elements = [5, 2, 1, 6, 7, 7, 6]
+    # last_elements.pop(1)
+    # print(last_elements)
